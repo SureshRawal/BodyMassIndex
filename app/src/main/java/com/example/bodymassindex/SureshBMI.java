47e6b550fbc5ee98e870d2bcf96d74ac000aa732
SureshRawal21 <rxurace21@yahoo.com>
2019-04-12 23:24:33 +0545

    2nd commit

---
+++ app/src/main/java/com/example/bodymassindex/SureshBMI.java
@@ -0,0 +1,27 @@
+package com.example.bodymassindex;
+
+public class SureshBMI {
+
+    private float height, weight;
+
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+    public float calculateBMI(){
+        return weight/(height*height);
+    }
+
+}
